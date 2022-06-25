f07cee3d4edbd3adad10f0b3b5df3761deb4a5c9
Muhammad Nafi Udin <sayanafime@gmail.com>
2022-06-25 08:43:55 +0700

    update

--- app/src/main/java/com/sayanafi/pmppractice/registrationScreen.java
+++ app/src/main/java/com/sayanafi/pmppractice/registrationScreen.java
@@ -13,61 +13,66 @@ import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.Spinner;
 
-public class registrationScreen extends AppCompatActivity
-    implements View.OnClickListener {
+public class registrationScreen extends AppCompatActivity {
 
-        EditText editName, editEmail, editAge, editDOB;
-        Spinner editCity;
-        RadioGroup gender;
-        RadioButton genderlist;
-        Button btnSubmit;
+    EditText editName, editEmail, editAge, editDOB;
+    Spinner editCity;
+    RadioGroup gender;
+    RadioButton genderlist;
+    Button btnSubmit;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            getSupportActionBar().hide();
-            setContentView(R.layout.activity_registration_scree);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_registration_scree);
 
-            editName = (EditText)findViewById(R.id.editName);
-            editEmail = (EditText)findViewById(R.id.editEmail);
-            editAge = (EditText)findViewById(R.id.editAge);
-            editDOB = (EditText)findViewById(R.id.editDOB);
-            editCity = findViewById(R.id.editCity);
-            gender = findViewById(R.id.editGender);
-            btnSubmit = (Button) findViewById(R.id.btnSubmit);
+        editName = findViewById(R.id.editName);
+        editEmail = findViewById(R.id.editEmail);
+        editAge = findViewById(R.id.editAge);
+        editDOB = findViewById(R.id.editDOB);
+        editCity = findViewById(R.id.editCity);
+        gender = findViewById(R.id.editGender);
+        btnSubmit = findViewById(R.id.btnSubmit);
 
 
-            //listener
-            btnSubmit.setOnClickListener(this);
-        }
+        btnSubmit.setOnClickListener(new View.OnClickListener() {
 
-        @Override
-        public void onClick(View v) {
-                Intent intent = new Intent(this,dataScreen.class);
-                intent.putExtra("name",editName.getText().toString());
-                intent.putExtra("mail",editEmail.getText().toString());
-                intent.putExtra("age",editAge.getText().toString());
-                intent.putExtra("dob",editDOB.getText().toString());
+            public void onClick(View view) {
+                int radioID = gender.getCheckedRadioButtonId();
+                genderlist = (RadioButton) findViewById(radioID);
+
+                Intent intent = new Intent(registrationScreen.this, dataScreen.class);
+                intent.putExtra("name", editName.getText().toString());
+                intent.putExtra("email", editEmail.getText().toString());
+                intent.putExtra("age", editAge.getText().toString());
+                intent.putExtra("dob", editDOB.getText().toString());
+                intent.putExtra("gender", genderlist.getText().toString());
                 intent.putExtra("city", editCity.getSelectedItem().toString());
-                intent.putExtra("gender",genderlist.getText().toString());
                 startActivity(intent);
-                finish();
-        }
+            }
+        });
 
-/*        Spinner spinner = findViewById(R.id.editCity);
-            if (spinner != null) {
+        Spinner spinner = findViewById(R.id.editCity);
+        if (spinner != null) {
             spinner.setOnItemSelectedListener((AdapterView.OnItemSelectedListener) this);
         }
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.city, android.R.layout.simple_spinner_item);
-            adapter.setDropDown
-                    (android.R.layout.simple_spinner_dropdown_item);
+        adapter.setDropDownViewResource
+                (android.R.layout.simple_spinner_dropdown_item);
 
-            if (spinner != null) {
+        if (spinner != null) {
             spinner.setAdapter(adapter);
-        }*/
+        }
+    }
 
-    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {}
-    public void onNothingSelected(AdapterView<?> adapterView) {}
+    public void onRadioButtonClicked(View view) {
+    }
+
+    public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+    }
+
+    public void onNothingSelected(AdapterView<?> adapterView) {
+    }
+
 }
-
